12aa8ae77fa2b603b4d54c7d51967accffe2e151
Thymolzzw <1916268924@qq.com>
2021-05-05 15:52:50 +0800

    202105051552

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -29,7 +29,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="user_info.avatar" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -75,7 +75,8 @@ export default {
     ...mapGetters([
       'sidebar',
       'avatar',
-      'device'
+      'device',
+      'user_info',
     ])
   },
   methods: {
